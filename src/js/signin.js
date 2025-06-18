0c9415c5328b74f1b01f3daeb7319160cdd50f56
Gilnei Gonzo <45856866+gcgonzo@users.noreply.github.com>
2025-06-18 12:33:33 -0300

    Label configuration and input cleaning

---
+++ src/js/signin.js
@@ -0,0 +1,38 @@
+// Configurações da tela principal de Login
+
+const input = document.querySelectorAll("input")
+const usuario = document.getElementById("usuario")
+const senha = document.getElementById("senha")
+const userLabel = document.getElementById("userLabel")
+const senhaLabel = document.getElementById("senhaLabel")
+
+// Configuração que desloca o label para cima do input
+
+input.forEach(() => {
+  usuario.addEventListener("input", (e) => {
+    let valor = e.target.value
+    // console.log(valor)
+    if (valor.length > 0) {
+      userLabel.classList.add("active")
+    } else {
+      userLabel.classList.remove("active")
+    }
+  })
+  senha.addEventListener("input", (e) => {
+    let val = e.target.value
+    // console.log(valor)
+    if (val.length > 0) {
+      senhaLabel.classList.add("active")
+    } else {
+      senhaLabel.classList.remove("active")
+    }
+  })
+})
+
+// Limpar o input nome após atualizar a página
+
+window.addEventListener("unload", () => {
+  input.forEach((e) => {
+    e.value = ""
+  })
+})
