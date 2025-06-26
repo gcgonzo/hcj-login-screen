8b8f672f67c73dd97ac79c8c7ef2f0cb575fc25f
Gilnei Gonzo <45856866+gcgonzo@users.noreply.github.com>
2025-06-26 15:44:56 -0300

    Update javascript

--- src/js/home.js
+++ src/js/home.js
@@ -12,5 +12,5 @@ function sair() {
   localStorage.removeItem("token")
   localStorage.removeItem("userLogado")
   localStorage.removeItem("nome")
-  window.location.href = "/index.html"
+  window.location.href = "../../index.html"
 }
